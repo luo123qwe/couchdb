1bfd0db7586be143b3a106082e0b66cef7cd3f5d
Garren Smith <garren.smith@gmail.com>
2020-03-10 18:55:39 +0200

    update tests to work with mango on fdb

--- src/mango/test/13-stable-update-test.py
+++ src/mango/test/13-stable-update-test.py
@@ -12,6 +12,7 @@
 
 import copy
 import mango
+import unittest
 
 DOCS1 = [
     {
@@ -44,6 +45,7 @@ class SupportStableAndUpdate(mango.DbPerClass):
         self.db.create_index(["name"], ddoc="foo")
         self.db.save_docs(copy.deepcopy(DOCS1))
 
+    @unittest.skip("this FDB doesn't support this")
     def test_update_updates_view_when_specified(self):
         docs = self.db.find({"name": "Eddie"}, update=False)
         assert len(docs) == 0
